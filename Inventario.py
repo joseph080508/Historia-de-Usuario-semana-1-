5dda36e85bb0666f71e1d6bd369c20ee9a667078
joseph080508 <joseph.080505@gmail.com>
2026-03-13 12:59:35 -0500

    Cambiando los comentarios a ingles

--- Inventario.py
+++ Inventario.py
@@ -1,13 +1,13 @@
-#Inventario del producto y pregunta al usuario por el nombre del producto, precio y cantidad. 
+#Inventory of the product and asks the user for the product name, price and quantity. 
 product = str(input("Enter the product name: "))
 price = float(input("Enter the product price: "))
 quantity = int (input("Enter the product quantity: "))
-#Validando que el precio y la cantidad sean no negativos y que el nombre del producto contenga solo letras
+#Validating that the price and quantity are non-negative and that the product name contains only letters
 if price < 0 or quantity < 0:
      print("The price and quantity must be non-negative. Please try again.")
 elif product.isalpha() == False:
         print("The product name must contain only letters. Please try again.")
-#Mostrabndo el costo total del producto        
+#Show the total cost of the product        
 Costo_total = price * quantity
 print("The total cost of the product is: ", Costo_total)
 # Mostrar los datos de ingreso y total 
@@ -16,4 +16,5 @@ print ("The price is: ", price)
 print ("The quantity is: ", quantity)
 print ("The total cost of the product is:  ", Costo_total)
  
-#Bueno el codigo es breve de explicar, se pide al usuario que productos, precio y cantidad desea para luego calcular el costo total del producto y mostrar los datos ingresados y el costo total. Se valida que el precio y la cantidad sean no negativos y que el nombre del producto contenga solo letras.
+ #the program should also display the total cost of the product, which is calculated by multiplying the price by the quantity.
+ 
