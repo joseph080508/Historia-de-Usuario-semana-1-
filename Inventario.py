c6e5983158ee1107260fffe896e2d1107543bbf5
joseph080508 <joseph.080505@gmail.com>
2026-03-13 12:48:27 -0500

    Cambiando diagrama de flujo y inventario  a ingles

--- Inventario.py
+++ Inventario.py
@@ -1,19 +1,19 @@
 #Inventario del producto y pregunta al usuario por el nombre del producto, precio y cantidad. 
-producto = str(input("Ingrese el nombre del producto: "))
-precio = float(input("Ingrese el precio del producto: "))
-cantidad = int (input("Ingrese la cantidad del producto: "))
+product = str(input("Enter the product name: "))
+price = float(input("Enter the product price: "))
+quantity = int (input("Enter the product quantity: "))
 #Validando que el precio y la cantidad sean no negativos y que el nombre del producto contenga solo letras
-if precio < 0 or cantidad < 0:
-     print("El precio y la cantidad deben ser no negativos. Por favor, intente de nuevo.")
-elif producto.isalpha() == False:
-        print("El nombre del producto debe contener solo letras. Por favor, intente de nuevo.")
+if price < 0 or quantity < 0:
+     print("The price and quantity must be non-negative. Please try again.")
+elif product.isalpha() == False:
+        print("The product name must contain only letters. Please try again.")
 #Mostrabndo el costo total del producto        
-Costo_total = precio * cantidad
-print("el costo total del producto es: ", Costo_total)
+Costo_total = price * quantity
+print("The total cost of the product is: ", Costo_total)
 # Mostrar los datos de ingreso y total 
-print ("el producto es: ", producto)
-print ("el precio es de: ", precio)
-print ("la cantidad es: ", cantidad)
-print ("el costo total del producto es: ", Costo_total)
+print ("The product is: ", product)
+print ("The price is: ", price)
+print ("The quantity is: ", quantity)
+print ("The total cost of the product is:  ", Costo_total)
  
 #Bueno el codigo es breve de explicar, se pide al usuario que productos, precio y cantidad desea para luego calcular el costo total del producto y mostrar los datos ingresados y el costo total. Se valida que el precio y la cantidad sean no negativos y que el nombre del producto contenga solo letras.
