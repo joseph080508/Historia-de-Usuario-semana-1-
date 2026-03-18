d0bc3803453662e33c361d6b694b4d33f5c265a9
joseph080508 <joseph.080505@gmail.com>
2026-03-18 12:25:20 -0500

    Actualizacion 2

--- Inventario.py
+++ Inventario.py
@@ -1,25 +1,2 @@
-Validation = True
 
-while Validation: 
-    try:
-        product = str(input("Enter the product name: "))
-        price = float(input("Enter the product price: "))
-        quantity = int (input("Enter the product quantity: "))
-    
-    
-        if price < 0 or quantity < 0:
-            print("The price and quantity must be non-negative. Please try again.")
-        elif not product.isalpha():
-            print("The product name must contain only letters. Please try again.")
-        else:
-            Costo_total = price * quantity
-            print("----------TOTAL----------")
-            print("The total cost of the product is: ", Costo_total)
-            print ("The product is: ", product)
-            print ("The price is: ", price)
-            print ("The quantity is: ", quantity)
-            print ("The total cost of the product is:  ", Costo_total)
-            
-            validation = False
-    except ValueError: 
-        print("invalid input, please enter number of price or quantity")
+
---
+++ menuV2.py
@@ -0,0 +1,83 @@
+
+
+inventario = []
+
+print("Welcome The inventory")
+
+is_running = True
+
+while is_running:
+    print("\n1. Add product")
+    print("\n2. Show Inventory")
+    print("\n3. calculate statistics")
+    print("\n4. Exit")
+    
+    try:
+        option = int(input("Enter the option: "))
+        
+        if option == 1:
+            product = str(input("Enter the product name: "))
+            price = float(input("Enter the product price: "))
+            quantity = int (input("Enter the product quantity: "))
+            
+            if price < 0 or quantity < 0:
+                print("The price and quantity must be non-negative. Please try again.")
+                
+            elif not product.isalpha():
+                print("The product name must contain only letters. Please try again.")
+            
+            elif product.strip():  # Verifica que no esté vacío
+                sale = {
+                 "product": product,
+                 "price": price,
+                 "quantity": quantity
+                 }
+                inventario.append(sale)
+                print(f"Product added successfully!")
+                
+        elif option == 2:
+            if not inventario:
+                print ("Inventory is empty ")
+            else:
+                print("-------INVENTORY--------")
+                for item in inventario:
+                    print ("The product is: ", item["product"])
+                    print ("The price is: ", item["price"])
+                    print ("The quantity is: ", item["quantity"])
+                    print("---------------------")
+            
+            
+        elif option == 3:
+            if not inventario:
+                print ("No products to caculate")
+            else: 
+                total = 0 
+                print("----------TOTAL----------")
+                print()
+        
+                for item in inventario:
+                    costo = item["price"] * item ["quantity"]
+                    total += costo
+                    print ("The product is: ", item["product"])
+                    print ("The price is: ", item["price"])
+                    print ("The quantity is: ", item["quantity"])
+                    print ("The total:  ", costo)
+            
+                    print ("TOTAL INVENTORY VALUE: ", total)
+            
+             
+                
+           
+            
+             
+        elif option == 4:
+            print("Goodbye!")
+            is_running = False
+        else:
+            print("Please choose 1,2,3 or 4")
+            
+    except ValueError:
+        print("Please write a valid number") 
+                        
+                        
+    
