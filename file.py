3c4922032d21041d9b92bca6099682c18b04ede8
joseph080508 <joseph.080508@gmail.com>
2026-03-25 22:55:23 -0500

    agregando funcion de guardar
    Implement a function to save inventory data to a CSV file.

--- file.py
+++ file.py
@@ -1,1 +1,22 @@
 import csv
+
+def save_csv(inventory, path):
+    if len(inventory) == 0:
+        print("No data to save")
+        return
+
+    try:
+        file = open(path, "w", newline="")
+        writer = csv.writer(file)
+
+        writer.writerow(["product", "price", "quantity"])
+
+        for item in inventory:
+            writer.writerow([item["product"], item["price"], item["quantity"]])
+
+        file.close()
+        print("Saved")
+
+    except:
+        print("Error saving file")
+  
