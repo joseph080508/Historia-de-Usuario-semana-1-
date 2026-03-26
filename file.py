25bf5aa9364afabae97e164f7dfa9d0ef118dbdd
joseph080508 <joseph.080508@gmail.com>
2026-03-25 23:03:09 -0500

    Implementen funcion load_csv en archivo
    Added a function to load CSV data into inventory.

--- file.py
+++ file.py
@@ -20,3 +20,37 @@ def save_csv(inventory, path):
     except:
         print("Error saving file")
   
+def load_csv(path):
+    inventory = []
+
+    try:
+        file = open(path, "r")
+        reader = csv.reader(file)
+
+        header = next(reader)
+
+        for row in reader:
+            try:
+                product = row[0]
+                price = float(row[1])
+                quantity = int(row[2])
+
+                data = {
+                    "product": product,
+                    "price": price,
+                    "quantity": quantity
+                }
+
+                inventory.append(data)
+
+            except:
+                print("Error in row")
+
+        file.close()
+        print("Loaded")
+
+        return inventory
+
+    except:
+        print("Error loading file")
+        return []
